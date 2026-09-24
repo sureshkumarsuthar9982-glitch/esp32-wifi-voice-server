bc61c47bb7aef07312c842a3c2e67ce134617746
sureshkumarsuthar9982-glitch <sureshkumarsuthar9982@gmail.com>
2026-09-24 17:40:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,17 +4,41 @@ const app = express();
 
 app.use(express.json());
 
+let latestCommand = "";
+
+// Server check
 app.get("/", (req, res) => {
-  res.send("ESP32 WiFi Voice Server is running");
+  res.send("ESP 32 Wi-Fi voice control server is running");
 });
 
+// Voice app sends command here
 app.post("/command", (req, res) => {
+
   const command = req.body.command;
 
   console.log("Command received:", command);
 
+  if (command) {
+    latestCommand = command.toString().toLowerCase().trim();
+  }
+
   res.json({
     success: true,
+    command: latestCommand
+  });
+});
+
+// ESP32 gets the latest command here
+app.get("/command", (req, res) => {
+
+  const command = latestCommand;
+
+  // Command read hone ke baad clear kar do
+  latestCommand = "";
+
+  console.log("ESP32 command:", command);
+
+  res.json({
     command: command
   });
 });
